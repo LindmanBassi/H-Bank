cd47b8f957b7bee0a74e250f3d24406723205c01
Henrique Lindman Bassi <148555934+LindmanBassi@users.noreply.github.com>
2025-07-23 07:16:46 -0300

    criacao do projeto

---
+++ src/main/java/br/com/bassi/h_bank/HBankApplication.java
@@ -0,0 +1,13 @@
+package br.com.bassi.h_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bassi/h_bank/HBankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bassi.h_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
